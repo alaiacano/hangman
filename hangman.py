83b34a514cb2e9ea32b2d277e88ae852ceb87720
Adam Laiacano <adam@tumblr.com>
2012-07-31 21:29:28 -0400

    example of guessing dirty words

--- hangman.py
+++ hangman.py
@@ -1,13 +1,25 @@
 from naive_guesser import NaiveGuesser
+import urllib2, re
 
 if __name__ == '__main__':
 
-    for i in xrange(10):
+    # Get the total number of guesses for every word 
+    # in Jacob Bijani's list of obscenities.
+
+    dirty_words = urllib2.urlopen('https://raw.github.com/jake/collections/master/profanities.txt').read()
+    dirty_words = [i.strip() for i in dirty_words.split('\n')]
+
+    total_guesses = 0
+    for i, word in enumerate(dirty_words):
         G = NaiveGuesser()
-        word = G.new_word()
+
+        # keep only letters and apostrophe
+        word = re.sub(r"[^a-z']", "", word)
 
         stats = G.guess_word(word, verbose=False)
-        for k,v in stats.iteritems():
-            print "%s : %s" % (k.upper(), v)
-        print ''
+        total_guesses += stats['guess_count']
 
+        print "Word %s : %s Guesses (%s) : %s" % (i, word, stats['guess_count'], stats['guesses'])
+
+    print "TOTAL WORDS:", len(dirty_words)
+    print "TOTAL GUESSES:", total_guesses
